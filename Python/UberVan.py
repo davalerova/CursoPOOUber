e5b7050aeb2f50bba3b36212a4d7c3d29ab036ad
davalerova <dvdovni@gmail.com>
2021-01-10 11:51:00 -0500

    Se crea herencia en Python

---
+++ Python/UberVan.py
@@ -0,0 +1,12 @@
+from car import Car
+
+class UberVan(Car):
+
+    type_car_acepted = []
+    seat_material = []
+
+    def __init__(self, license, driver, type_car_acepted, seat_material):
+        super().__init__(license, driver)
+        self.type_car_acepted = type_car_acepted
+        self.seat_material = seat_material
+    
