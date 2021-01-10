ab9ac809e40784b1ae008191a2b323d6f429db35
davalerova <dvdovni@gmail.com>
2021-01-10 11:58:39 -0500

    Se crea herencia en javaScript

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("AW456", new Account("David Valero", "efsd43sdf"));
 car.passengers = 4;
 car.printDataCar();
+
+var uberX = new UberX("DDFD343", new Account("David Valero", "243432423"), "Chevrolet", "Spark");
+uberX.passengers = 4;
+uberX.printDataCar();
